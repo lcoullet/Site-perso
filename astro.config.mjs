5ace24fb5607c919ddabaddbe0c353ddee229bdd
Ludovic Coullet <lcoullet@gmail.com>
2023-06-16 13:36:58 +0200

    images et seo et compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), astroImageTools, compress()]
 });
