2f62be8e78910f0fcaaf0203e26efcdc26d0ea7f
Ludovic Coullet <lcoullet@gmail.com>
2025-01-22 17:20:17 +0100

    tailwind baby

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ludovic.coullet.net',
-
+  site: "https://ludovic.coullet.net",
+  integrations: [tailwind()],
 });
