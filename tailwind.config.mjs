0b04218f470fcb944f03d8d7ef61d6287e9278b3
Ludovic Coullet <lcoullet@gmail.com>
2025-01-30 11:05:42 +0100

    flyonui + menu

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", './node_modules/flyonui/dist/js/*.js'],
   theme: {},
   extend: {},
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require('flyonui'), require('flyonui/plugin')],
 };
