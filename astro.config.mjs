caa7f4597c8435838b3e0143b553c1a7e904761c
Ludovic Coullet <ludoviccoullet@MacBook-Air-de-Ludovic-2.local>
2026-03-11 00:43:12 +0100

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ludovic.coullet.net",
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
