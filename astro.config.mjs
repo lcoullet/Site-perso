bf0b72227a07a312fe916e67d85a24ebef8985e1
Ludovic Coullet <lcoullet@gmail.com>
2026-05-20 00:27:45 +0200

    perf: optimize fonts loading and add canonical, social tags & sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ludovic.coullet.net",
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       themes: {
