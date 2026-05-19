1fbd8221099643dc58141338b2ead861137410ee
Ludovic Coullet <lcoullet@gmail.com>
2026-05-19 22:35:13 +0200

    blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ludovic.coullet.net",
+  integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
